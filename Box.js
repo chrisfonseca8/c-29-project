69b0e4658a1f3883feb7b25ba4611dd2a9549b6d
chrisfonseca8 <65946600+chrisfonseca8@users.noreply.github.com>
2020-08-16 14:57:55 +0530

    Add files via upload

---
+++ Box.js
@@ -0,0 +1,18 @@
+class Box {
+    constructor(x,y,width,height){
+        var options = {
+            'restitution':0.2,
+            'friction':0.01,
+            'density':0.01
+        }
+        this.body = Bodies.rectangle(x,y,width,height,options);
+        this.width = width;
+        this.height = height;
+        World.add(world,this.body);
+    }
+    display(){
+        var pos = this.body.position;
+        fill("purple");
+        rect(pos.x,pos.y,this.width,this.height);
+    }
+}
